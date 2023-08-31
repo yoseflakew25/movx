f1a1e123755835bd27df7a17e2be10338748c165
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-08-31 22:17:41 +0300

    first commit

---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <div className="footer">
+    <span>
+      Made by &nbsp;
+      <a href="https://github.com/yoseflakew25" target="_blank">
+        Yosef lakew
+      </a>
+      &nbsp; with &nbsp;
+      <i className="fa fa-heart" />
+    </span>
+  </div>
+);
+
+export default Footer;
